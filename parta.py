9cf93e9d92948ef349c95708bb80fcbc809e3716
anatolygs <anatolygs@gmail.com>
2021-09-02 00:30:41 +0300

    add

---
+++ parta.py
@@ -0,0 +1,5 @@
+k1 = int(input())
+k2 = int(input())
+k3 = int(input())
+
+print(round((k1+k2+k3)/2))
---
+++ time.py
@@ -0,0 +1,9 @@
+#https://pythontutor.ru/lessons/inout_and_arithmetic_operations/
+
+# t=int(input())
+#
+# chas = t//60
+# min = t-(chas*60)
+# print(chas//24)
+# print(min)
+
