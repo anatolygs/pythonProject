c1ca6ac6695933ce55d646b3479e4a99a612def9
anatoly <anatolygs@gmail.com>
2021-09-02 17:35:18 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	time.py

--- parta.py
+++ parta.py
@@ -2,4 +2,4 @@ k1 = int(input())
 k2 = int(input())
 k3 = int(input())
 
-print(round((k1+k2+k3)/2))
+print(round((k1+k2+k3)//2))
---
+++ shnur.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=int(input())
+l=int(input())
+n=int(input())
+
+# print(((a+b)*n)+l)
+print((((a+b)*2)*n)+l)
+print(((a*n)+((b*n)-1))+l)
+print((a*(n-1)+(b*(n-2)+(l*2))))
