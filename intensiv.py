bdaeee519b0996ff555df6a280bd31931263bb80
anatoly <anatolygs@gmail.com>
2021-09-05 09:41:29 +0300

    add

---
+++ intensiv.py
@@ -0,0 +1,16 @@
+# a = 0
+# x = []
+# for i in range(10):
+#     ai = 'a' + str(i)
+#     ai = int(input())
+#     x.append(ai)
+# print(sum(x))
+
+# a = int(input())
+# x = []
+# for i in range(a):
+#     ai = 'a' + str(i)
+#     ai = int(input())
+#     x.append(ai)
+# print(sum(x))
+
--- time.py
+++ time.py
@@ -1,6 +1,5 @@
 #https://pythontutor.ru/lessons/inout_and_arithmetic_operations/
 
-<<<<<<< HEAD
 # t=int(input())
 #
 # chas = t//60
@@ -8,11 +7,10 @@
 # print(chas//24)
 # print(min)
 
-=======
+
 t=int(input())
 
 chas = t//60
 min = t-(chas*60)
 print(chas)
 print(min)
->>>>>>> origin/master
